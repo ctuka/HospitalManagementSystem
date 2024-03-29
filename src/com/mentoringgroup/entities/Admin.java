2a5b70e774191c0c1de68a369e2c746e9fb90f48
ctuka <ozkan.mkl.ozkanQgmail.com>
2024-03-29 16:31:56 +0100

    admin v1

--- src/com/mentoringgroup/entities/Admin.java
+++ src/com/mentoringgroup/entities/Admin.java
@@ -1,4 +1,26 @@
 package com.mentoringgroup.entities;
 
+import java.util.Scanner;
+
 public class Admin {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void adminLogin(){
+        String id;
+        String password;
+        String select;
+
+        do {
+            System.out.println("Please enter your Id number");
+            id= scan.nextLine();
+            System.out.println("Please enter your password");
+            password=scan.nextLine();
+            select=scan.nextLine();
+
+
+        }while(select.equalsIgnoreCase("q"));
+
+
+    }
 }
