41981697747751787a0cb11927eb55c892ecbd58
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-03-29 20:21:46 +0100

    patient v2

--- src/com/mentoringgroup/entities/Patient.java
+++ src/com/mentoringgroup/entities/Patient.java
@@ -4,5 +4,6 @@ public class Patient {
 
     public static void loginPage(){
         System.out.println("Welcome to our Hospital");
+        System.out.println("Please enter your id and password");
     }
 }
